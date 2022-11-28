38d48a34ce06147c17f13917ed0578744a41fce0
Victor Sartor <sartor.santos@gmail.com>
2022-11-28 16:50:58 -0300

    Jump
    Double jump, configurado

--- TBAO - 2D/Assets/Scripts/player.cs
+++ TBAO - 2D/Assets/Scripts/player.cs
@@ -6,7 +6,11 @@ public class player : MonoBehaviour
 {
 
     public float Speed;
-    public float jumpForce; 
+    public float jumpForce;
+
+    public bool isJumping;
+    public bool doubleJump;
+
     private Rigidbody2D rig;
 
     // Start quando inicia o 1° frame apenas UMA VEZ
@@ -36,7 +40,38 @@ public class player : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+          
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+
+        }
+    }
+
+
 }
