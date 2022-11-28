23fc41cc3bc34fe22f88789bedb7dbc2a92fa925
Victor Sartor <sartor.santos@gmail.com>
2022-11-28 20:14:14 -0300

    Inimigo

--- TBAO - 2D/Assets/Scripts/player_combat.cs
+++ TBAO - 2D/Assets/Scripts/player_combat.cs
@@ -6,6 +6,10 @@ public class player_combat : MonoBehaviour
 {
     public Animator animator;
 
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
@@ -19,8 +23,21 @@ public class player_combat : MonoBehaviour
         //dar ataque
         animator.SetTrigger("Attack");
         //detectar inimigos no range
-        //dar dano
+        Collider2D [] hitEnemis = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
+        //dar dano
+        foreach(Collider2D enemy in hitEnemis)
+        {
+            Debug.Log("We hit" + enemy.name);
+        }
     }
 
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }
